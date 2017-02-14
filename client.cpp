1d93367b46c232fb6b2b488e52270c9d56e40c94
Isamar Lopez-Rodriguez <isamar.lo15425@gmail.com>
2017-02-14 07:59:07 -0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,9 +1,7 @@
 //Client file
 //******************************************************************
 //
-//   Programador : Isamar López Rodríguez CCOM 3034 Sección 0U1
-//   Num. Est.   : 801-08-3174            Primer Semestre 2016-2017
-//   Asignación 3                         Prof. R. J. Colorado
+//   Programador : Isamar López Rodríguez 
 //   Archivo     : client.cpp             Fecha : 08/09/16
 //
 //******************************************************************
